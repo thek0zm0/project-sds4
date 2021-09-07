95cb8af6068479027c03534afad464e044dea713
Lucas Moraes <lucasmoraes@alunos.utfpr.edu.br>
2021-09-07 11:23:15 -0300

    Project created

---
+++ backend/src/main/java/com/sdsk/dsksales/DsksalesApplication.java
@@ -0,0 +1,13 @@
+package com.sdsk.dsksales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsksalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsksalesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/sdsk/dsksales/DsksalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdsk.dsksales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsksalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
